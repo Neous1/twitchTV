e11d17be3fd69a3e6bd5bc70c8b48b72f2fbaeac
Neous1 <Neous1@users.noreply.github.com>
2017-08-15 14:26:02 -0400

    add logo to output

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "adam4813", "ThijsHS", "imaqtpie"]
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "adam4813", "ThijsHS", "imaqtpie", "loltyler1", "nl_Kripp"]
 
 //function is used to pull data (api call)
-function getData(type, user) {  
+function getData(type, user) {
     var url = "https://wind-bow.glitch.me/twitch-api/" + type + "/" + user;
 
     return $.getJSON(url)
@@ -16,23 +15,25 @@ users.forEach(function (user) {
 
 });
 
-function displayData(userData, streamsData){
-        console.log(userData);
-  for (var i = 0; i<users[i].length; i++){
-    // console.log(userData.stream);
-    // console.log(userData.name);
+function displayData(userData, streamsData) {
+    console.log(userData);
+    console.log(streamsData);
+    for (var i = 0; i < users[i].length; i++) {
+        // console.log(userData.stream);
+        // console.log(userData.name);
 
-    // console.log(userData.display_name);
-    // console.log(streamsData);
-    // $("#loc").html(loc)
-
-
-    var name = userData.name
-    var bio = userData.bio
+        // console.log(userData.display_name);
+        // console.log(streamsData);
+        // $("#loc").html(loc)
 
 
-    // $("#name").html(name)
-  }
-    $("#players").append("<li>"+"+'Name:"+name+""+bio+""+"</li>")
-//+"+_+"+
+        var logo = userData.logo
+        var name = userData.name
+        var bio = userData.bio
+
+
+        // $("#name").html(name)
+    }
+    $("#players").append("<li>" + ""+"<img src='"+logo+"' alt='"+ name + "'>"+"  " +"<strong>Name :" +" "+ name + "</strong>" + "<br>" + bio + "<br>" + "</li>")
+    //+"+_+"+
 }
